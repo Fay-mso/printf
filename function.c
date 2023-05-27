95417a2397b465372a500a4e2ab77fadd2b53f89
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-05-27 04:02:27 -0700

    function.c

---
+++ function.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*main - function that prints d and i
+*code for format specifier
+*Return: 0
+**/
+int main(void)
+{
+int years = 35;
+int b = 3030;
+printf("I am %d old\n", years);
+printf("%i\n", b);
+return (0);
+}
