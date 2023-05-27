bf4a6e59a8e866640aafcdd96ee783dac1c14de2
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-05-27 06:52:23 -0700

    function.c

--- function.c
+++ function.c
@@ -3,40 +3,13 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-*_printf- function that prints d and i
-*@format: format
+*main- function that prints d and i
 *code for format specifier
 *Return: 0
 **/
-int _printf(const char *format, ...)
+int main(void)
 {
-int i = 0;
-va_list(length);
-va_start(length, format);
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-if (*format == 'd' || *format == 'i')
-{
-int value = va_arg(length, int);
-i += printf("%d", value);
-}
-else
-{
-putchar(*format);
-i+= 2;
-putchar('%');
-}
-}
-else
-{
-putchar(*format);
-i++;
-}
-format++;
-}
-va_end(length);
-return (i);
+int len = 39;
+printf("Length:[%d, %i]\n", len, len);
+return (0);
 }
