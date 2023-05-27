0d38e18953966025084e519ec3e15c0bbd1cedb3
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-05-27 08:16:21 -0700

    function.c

--- function.c
+++ function.c
@@ -3,13 +3,40 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-*main- function that prints d and i
-*code for format specifier
-*Return: 0
+*print_int - prints the function
+*convert_size_number - coverts given number based on size
+*@width: specifies characters to be printed
+*@precision: specifies the maximum characters to be printed
+*@buffer: memory block to store data
+*@flags: modify functions
+*@size: allocates space
+*@args: arguments
+*Return: printed characters
 **/
-int main(void)
+int convert_size_number(int num, int size);
+int print_int(va_list args, char buffer[], int size)
 {
-int len = 39;
-printf("Length:[%d, %i]\n", len, len);
-return (0);
+unsigned long int n;
+int num = va_arg(args, int);
+int j = size - 2;
+num = convert_size_number(num, size);
+n = (unsigned long int)num;
+if (num == 0)
+{
+buffer[j--] = '0';
+}
+buffer[size - 1] = '\0';
+if (num < 0)
+{
+n = (unsigned long int)(-1 * num);
+}
+while (n > 0)
+{
+buffer[j--] = (n % 10) + '0';
+n /= 10;
+}
+{
+j++;
+}
+return (j);
 }
