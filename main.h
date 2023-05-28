c3d9f899fd904a6f5963cd36271425a2671fb779
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-05-28 06:48:35 -0700

    function.c

--- main.h
+++ main.h
@@ -1,9 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 void convert_size_number(unsigned int num);
+void print_buffer(char buf[], int *buff_ind);
+int print_char(va_list types);
+int print_string(va_list types);
+int print_percent(va_list types);
+int print_int(va_list types);
+int print_binary(va_list types);
 
 #endif /* MAIN_H */
 
