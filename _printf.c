833e030374832adf044ba2ee301f884f7aa3589e
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-05-28 02:36:12 -0700

    _print

--- _printf.c
+++ _printf.c
@@ -1,70 +1,60 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include "main.h"
-
-int _putchar(char c);
+#define BUFFSIZE 1024
+int get_width(int *j, const char *format, va_list args);
+int get_flags(const char *format, int *j);
+int get_precision(const char *format, int *j, va_list args);
+int get_size(const char *format, int *j);
 
 /**
- * _puts - writes a string to stdout
- * @str: the string to print
- *
- * Return: the number of characters printed
- */
-int _puts(const char *str)
+* print_buffer - prints buffer
+* @buf: buffer to be printed
+* @buff_ind: pointer to the buffer index
+*/
+void print_buffer(char buf[], int *buff_ind)
 {
-int i = 0;
-while (str && str[i])
-{
-_putchar(str[i]);
-i++;
-}
-return (i);
+if (*buff_ind > 0)
+write(1, buf, *buff_ind);
+*buff_ind = 0;
 }
 
 /**
- * _printf - custom printf function
- * @format: format string
- *
- * Return: number of characters printed (excluding null byte)
- */
+* _printf - prints string pointed out by format
+* @format: format string
+*
+* Return: number of characters printed (excluding null byte)
+*/
 int _printf(const char *format, ...)
 {
+int j = 0;
+int printed = 0, printed_chars = 0;
 va_list args;
-int count = 0;
+char buf[BUFFSIZE];
+int buff_ind = 0;
 va_start(args, format);
-while (format && *format)
+if (format == NULL)
+return (-1);
+for (j = 0; format && format[j] != '\0'; j++)
 {
-if (*format == '%')
+if (format[j] != '%')
 {
-format++;
-switch (*format)
-{
-case 'c':
-_putchar(va_arg(args, int));
-count++;
-break;
-case 's':
-count += _puts(va_arg(args, char *));
-break;
-case '%':
-_putchar('%');
-count++;
-break;
-default:
-_putchar('%');
-_putchar(*format);
-count += 2;
-break;
-}
+buf[buff_ind++] = format[j];
+if (buff_ind == BUFFSIZE)
+print_buffer(buf, &buff_ind);
+printed_chars++;
 }
 else
 {
-_putchar(*format);
-count++;
+print_buffer(buf, &buff_ind);
+j++;
+if (printed == -1)
+return (-1);
+printed_chars += printed;
 }
-format++;
 }
 va_end(args);
-return (count);
+print_buffer(buf, &buff_ind);
+return (printed_chars);
 }
 
