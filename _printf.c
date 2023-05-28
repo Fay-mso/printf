a08479e781e33a669c785a8f1751b046f82f6088
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-05-28 05:10:27 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -4,74 +4,54 @@
 #include "main.h"
 #include <string.h>
 #define BUFFSIZE 1024
-/**
- * print_buffer - Prints the contents of the buffer if it exists
- * @buffer: Array of characters
- * @buff_ind: Index at which to add next character, represents the length
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-if (*buff_ind > 0)
-{
-fwrite(buffer, sizeof(char), *buff_ind, stdout);
-}
-*buff_ind = 0;
-}
-void print_buffer(char buffer[], int *buff_ind);
 
-/**
- * custom_printf - Custom printf function
- * @format: Format string
- * @args: Variable arguments
- * Return: Number of characters printed
- */
-int custom_printf(const char *format, ...)
+#define BUFFSIZE 1024
+
+void print_buffer(char buf[], int *buff_ind)
 {
-int printed_chars = 0;
-char buffer[BUFFSIZE];
-int buff_ind = 0;
-va_list args;
-va_start(args, format);
-while (*format != '\0')
-{
-if (*format != '%')
-{
-buffer[buff_ind++] = *format;
-if (buff_ind == BUFFSIZE)
-{
-print_buffer(buffer, &buff_ind);
+    if (*buff_ind > 0)
+        write(1, buf, *buff_ind);
+    *buff_ind = 0;
 }
-putchar(*format);
-printed_chars++;
-}
-else
+
+void my_putchar(char c)
 {
-print_buffer(buffer, &buff_ind);
-format++; 
-switch (*format)
+    write(1, &c, 1);
+}
+
+int _printf(const char *format, ...)
 {
-case 'd':
-{
-int num = va_arg(args, int);
-printf("%d", num);
-printed_chars++;
-break;
-}
-case 's':
-{
-char *str = va_arg(args, char *);
-printf("%s", str);
-printed_chars += strlen(str);
-break;
-}
-default:
-break;
-}
-}
-format++;
-}
-print_buffer(buffer, &buff_ind);
-va_end(args);
-return printed_chars;
+    int j = 0;
+    int printed_chars = 0;
+    va_list args;
+    int buff_ind = 0;
+    char buf[BUFFSIZE];
+
+    va_start(args, format);
+
+    if (format == NULL)
+        return (-1);
+
+    for (j = 0; format && format[j] != '\0'; j++)
+    {
+        if (format[j] != '%')
+        {
+            buf[buff_ind++] = format[j];
+            if (buff_ind == BUFFSIZE)
+                print_buffer(buf, &buff_ind);
+            printed_chars++;
+        }
+        else
+	{
+            print_buffer(buf, &buff_ind);
+            j++;
+
+            printed_chars++;
+        }
+    }
+
+    va_end(args);
+    print_buffer(buf, &buff_ind);
+    return (printed_chars);
 }
 
