4f1dce8339b12d9885eb03e5aa0aff6cb8949b96
josiekarimi <josphinekarimi97@gmail.com>
2023-05-29 03:54:03 -0700

    changes

---
+++ _printf.c
@@ -0,0 +1,70 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+
+int _putchar(char c);
+
+/**
+ * _puts - writes a string to stdout
+ * @str: the string to print
+ *
+ * Return: the number of characters printed
+ */
+int _puts(const char *str)
+{
+int i = 0;
+while (str && str[i])
+{
+_putchar(str[i]);
+i++;
+}
+return (i);
+}
+
+/**
+ * _printf - custom printf function
+ * @format: format string
+ *
+ * Return: number of characters printed (excluding null byte)
+ */
+int _printf(const char *format, ...)
+{
+va_list args;
+int count = 0;
+va_start(args, format);
+while (format && *format)
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+_putchar(va_arg(args, int));
+count++;
+break;
+case 's':
+count += _puts(va_arg(args, char *));
+break;
+case '%':
+_putchar('%');
+count++;
+break;
+default:
+_putchar('%');
+_putchar(*format);
+count += 2;
+break;
+}
+}
+else
+{
+_putchar(*format);
+count++;
+}
+format++;
+}
+va_end(args);
+return (count);
+}
+
