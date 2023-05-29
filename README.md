73b3b03f74ce726e462a6f62078cc42744ef4caf
josiekarimi <josphinekarimi97@gmail.com>
2023-05-29 03:31:22 -0700

    restore

--- README.md
+++
@@ -1,2 +0,0 @@
-group printf project
-prinf readme
--- main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-int _printf(const char *format, ...);
-void convert_size_number(unsigned int num);
-
-#endif /* MAIN_H */
-
