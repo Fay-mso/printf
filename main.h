b0b3cfab7b601a3ff73af7d7bedcb6635e8677ba
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-05-30 11:59:31 -0700

    print_pointer.c

--- main.h
+++ main.h
@@ -6,5 +6,5 @@ int _printf(const char *format, ...);
 int _print_integer(int n);
 int printf_int(va_list args);
 int printf_dec(va_list args);
-
+int printf_pointer(va_list val);
 #endif /* MAIN_H */
