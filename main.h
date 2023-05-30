f585c6f1cc2578b308a5a7a5048422e49c3a5443
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-05-30 11:38:24 -0700

    print_int.c

--- main.h
+++ main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdarg.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _print_integer(int n);
+int printf_int(va_list args);
+int printf_dec(va_list args);
 
 #endif /* MAIN_H */
---
+++ mmain.h
@@ -0,0 +1,4 @@
+#ifndef get_flags_h
+#define get_flags_h
+int get_flags(const char *format, int *i)
+#endif
